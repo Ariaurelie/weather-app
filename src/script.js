5db9a03cb28aab528c8e51b9881652f0727e83ba
Ariaurelie <dzholtikova@st.swps.edu.pl>
2022-08-06 13:01:43 +0300

    Added the forecast

--- src/script.js
+++ src/script.js
@@ -16,6 +16,40 @@ function formatDate() {
   return `${day} ${hours}:${minutes}`;
 }
 
+function formatDay(timestamp) {
+let date = new Date(timestamp * 1000);
+let day = date.getDay();
+
+let days = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
+
+return days[day];
+}
+
+function displayForecast(response) {
+let forecast = response.data.daily;
+
+  let forecastElement = document.querySelector('#forecast');
+
+    let forecastHTML = `<div class="row">`;
+  let days = ['Fri', 'Sat', 'Sun', 'Mon'];
+  forecast.forEach(function(forecastDay, index) {
+    if (index < 6) {
+     forecastHTML =
+       forecastHTML +
+       `
+    <div class="col-2">
+              ${formatDay(forecastDay.dt)} <br />
+              <img src="http://openweathermap.org/img/wn/${forecastDay.weather[0].icon}@2x.png" alt="" width="42">
+              <br />
+              <strong>${Math.round(forecastDay.temp.max)}°</strong> ${Math.round(forecastDay.temp.min)}°
+            </div>
+            `;}
+            
+  })
+            forecastHTML = forecastHTML + `</div>`;
+            forecastElement.innerHTML = forecastHTML;
+}
+
 function searchCity(city) {
   console.log(city);
   let apiKey = '76a24252f5d2d956e723e94ad0977c83';
@@ -26,6 +60,12 @@ function searchCity(city) {
 function getCurrentLocation(event) {
   event.preventDefault();
   navigator.geolocation.getCurrentPosition(searchLocation);
+}
+
+function getForecast(coordinates) {
+let apiKey = '76a24252f5d2d956e723e94ad0977c83';
+let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
+axios.get(apiUrl).then(displayForecast);
 }
 
 function displayWeatherCondition(response) {
@@ -39,6 +79,8 @@ function displayWeatherCondition(response) {
   document.querySelector('#icon').setAttribute('src', `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`);
 
   celciusTemperature = response.data.main.temp;
+
+  getForecast(response.data.coord);
 }
 
 function searchLocation(position) {
